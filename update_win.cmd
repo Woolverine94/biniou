100ce682d1d76b6c6505f9bfb4e9a906c510ce75
Woolverine94 <woolverine94@proton.me>
2023-10-04 01:36:45 +0200

    Bugfix : multiples deprecation warnings during install

--- update_win.cmd
+++ update_win.cmd
@@ -8,6 +8,7 @@ git pull
 
 echo "Biniou env update"
 call venv.cmd
+python -m pip install --upgrade wheel
 python -m pip install --upgrade torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 python -m pip install --upgrade https://github.com/abetlen/llama-cpp-python/releases/download/v0.2.7/llama_cpp_python-0.2.7-cp311-cp311-win_amd64.whl
 python -m pip install --upgrade -r requirements.txt
