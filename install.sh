e8724d9aeca107410e5b3cd3f3a287c1f69bc5e9
Woolverine94 <woolverine94@proton.me>
2023-12-13 04:20:26 +0100

    Bugfix install.sh and update.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PYVER=$(python --version|sed -ne 's/^.* \([0-9]\)\.\([0-9]*\)\.[0-9]*/\1\2/p')
 
 mkdir -p ./outputs
 mkdir -p ./ssl
@@ -15,7 +16,7 @@ source ./env/bin/activate
 pip install -U pip
 pip install wheel
 pip install torch==2.1.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
-pip install https://github.com/abetlen/llama-cpp-python/releases/download/v0.2.22/llama_cpp_python-0.2.22-cp311-cp311-manylinux_2_17_x86_64.whl
+pip install https://github.com/abetlen/llama-cpp-python/releases/download/v0.2.22/llama_cpp_python-0.2.22-cp${PYVER}-cp${PYVER}-manylinux_2_17_x86_64.whl
 # FORCE_CMAKE=1 pip install llama-cpp-python==0.2.13
 pip install -r requirements.txt
 
--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+PYVER=$(python --version|sed -ne 's/^.* \([0-9]\)\.\([0-9]*\)\.[0-9]*/\1\2/p')
+
 echo "Biniou update ..."
 git pull
 
@@ -7,7 +9,7 @@ source ./env/bin/activate
 pip install -U pip
 pip install -U wheel
 pip install -U torch==2.1.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
-pip install -U https://github.com/abetlen/llama-cpp-python/releases/download/v0.2.22/llama_cpp_python-0.2.22-cp311-cp311-manylinux_2_17_x86_64.whl
+pip install -U https://github.com/abetlen/llama-cpp-python/releases/download/v0.2.22/llama_cpp_python-0.2.22-cp${PYVER}-cp${PYVER}-manylinux_2_17_x86_64.whl
 # FORCE_CMAKE=1 pip install -U llama-cpp-python
 pip install -U -r requirements.txt
 
