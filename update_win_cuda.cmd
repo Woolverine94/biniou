efd85874dfd46788c870ddbd379b221d47418888
Woolverine94 <woolverine94@proton.me>
2023-12-15 05:10:24 +0100

    Adding scripts for enabling and updating CUDA support

---
+++ update_win_cuda.cmd
@@ -0,0 +1,17 @@
+REM ****************************
+REM *** BINIOU UPDATE SCRIPT ***
+REM ****************************
+
+echo "Biniou update ..."
+git pull
+
+echo "Biniou env update"
+call venv.cmd
+python -m pip install --upgrade pip
+python -m pip install --upgrade wheel
+python -m pip install --upgrade torch==2.1.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+python -m pip install --upgrade llama-cpp-python
+python -m pip install --upgrade -r requirements.txt
+echo "Update finished ! You could now launch biniou by double-clicking %userprofile%\biniou\webui.cmd"
+pause
+
