78cc6eaeb11eedc505d7efb7fe24d1ac9b6b5b24
Woolverine94 <woolverine94@proton.me>
2024-03-07 02:07:52 +0100

    Typos in oci-debian.sh

--- oci-debian.sh
+++ oci-debian.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 echo ">>>[biniou oci 🧠]: biniou one-click installer for Debian based-distributions"
-echo ">>>[biniou 🧠]: Installing prerequisites"
+echo ">>>[biniou oci 🧠]: Installing prerequisites"
 if [ $(lsb_release -si|grep Debian) != "" ]
   then
     su root -c "apt -y install git pip python3 python3-venv gcc perl make ffmpeg openssl google-perftools"
   else
     sudo apt -y install git pip python3 python3-venv gcc perl make ffmpeg openssl google-perftools
 fi
-echo ">>>[biniou 🧠]: Cloning repository"
+echo ">>>[biniou oci 🧠]: Cloning repository"
 git clone https://github.com/Woolverine94/biniou.git --branch main
-echo ">>>[biniou 🧠]: Installing Virtual environment"
+echo ">>>[biniou oci 🧠]: Installing Virtual environment"
 cd ./biniou
 ./install.sh
-echo "Installation finished. Use ./webui.sh to launch biniou. Press enter to exit"
+echo ">>>[biniou oci 🧠]: Installation finished. Use ./webui.sh to launch biniou. Press enter to exit"
 read dummy
 exit 0
 
