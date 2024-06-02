ffdb382457f3bb283d28dc2bf796a111f3909516
Woolverine94 <woolverine94@proton.me>
2024-06-02 03:13:51 +0200

    Adding support for Ubuntu 24.04

--- oci-debian.sh
+++ oci-debian.sh
@@ -4,6 +4,10 @@ echo ">>>[biniou oci 🧠 ]: Installing prerequisites"
 if [ "$(lsb_release -si|grep Debian)" != "" ]
   then
     su root -c "apt -y install git pip python3 python3-venv gcc perl make ffmpeg openssl libtcmalloc-minimal4"
+  elif [ "$(cat /etc/os-release|grep VERSION_CODENAME|grep noble)" != "" ]
+    then
+      sudo add-apt-repository -y ppa:deadsnakes/ppa
+      sudo apt -y install git pip python3.11 python3.11-venv python3.11-dev gcc perl make ffmpeg openssl libtcmalloc-minimal4
   else
     sudo apt -y install git pip python3 python3-venv gcc perl make ffmpeg openssl libtcmalloc-minimal4
 fi
@@ -11,8 +15,12 @@ echo ">>>[biniou oci 🧠 ]: Cloning repository"
 git clone --branch main https://github.com/Woolverine94/biniou.git
 echo ">>>[biniou oci 🧠 ]: Installing Virtual environment"
 cd ./biniou
-./install.sh
+if [ "$(cat /etc/os-release|grep VERSION_CODENAME|grep noble)" != "" ]
+  then
+    ENV_BINIOU_PYTHON_VER="python3.11" ./install.sh
+  else
+    ./install.sh
+fi
 echo ">>>[biniou oci 🧠 ]: Installation finished. Use cd biniou && ./webui.sh to launch biniou. Press enter to exit"
 read dummy
 exit 0
-
