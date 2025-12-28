cbe6c2f49a7b2be03c56aa3ddcaad9f43f1b8830
Woolverine94 <woolverine94@proton.me>
2025-12-28 15:15:45 +0100

    Add support for nllb model facebook/nllb-200-1.3B

--- ressources/nllb.py
+++ ressources/nllb.py
@@ -22,6 +22,7 @@ for filename in os.listdir(model_path_nllb):
 model_list_nllb_builtin = [
     "facebook/nllb-200-distilled-600M",
     "facebook/nllb-200-distilled-1.3B",
+    "facebook/nllb-200-1.3B",
     "facebook/nllb-200-3.3B",
 ]
 
