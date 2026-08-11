c3e140bbe41b93613ae5435fd0df60052bd2bb65
Woolverine94 <woolverine94@proton.me>
2026-08-11 18:41:17 +0200

    🔥 Add support for Real ESRGAN model schwgHao/RealESRGAN_x4plus

--- ressources/r_esrgan.py
+++ ressources/r_esrgan.py
@@ -21,6 +21,7 @@ model_list_resrgan = [
     "RealESRGAN_x2.pth",
     "RealESRGAN_x4.pth",
     "RealESRGAN_x8.pth",
+    "RealESRGAN_x4plus.pth",
 ]
 
 @metrics_decoration
@@ -28,18 +29,25 @@ def image_resrgan(
     modelid_resrgan,
     scale_resrgan, 
     img_resrgan, 
-    use_gfpgan_resrgan, 
+    use_gfpgan_resrgan,
     progress_resrgan=gr.Progress(track_tqdm=True)
     ):
 
     print(">>>[Real ESRGAN 🔎]: starting module")
 
-    huggingface_hub.cached_download = lambda *args, **kwargs: hf_hub_download("sberbank-ai/Real-ESRGAN", modelid_resrgan, local_dir=model_path_resrgan, **kwargs)
+    huggingface_hub.cached_download = lambda *args, **kwargs: hf_hub_download(
+		"schwgHao/RealESRGAN_x4plus" if modelid_resrgan == "RealESRGAN_x4plus.pth" else "sberbank-ai/Real-ESRGAN",
+		modelid_resrgan,
+		local_dir=model_path_resrgan,
+		**kwargs
+    )
+
     from RealESRGAN import RealESRGAN
 
     hf_hub_download("sberbank-ai/Real-ESRGAN", "RealESRGAN_x2.pth", local_dir=model_path_resrgan)
     hf_hub_download("sberbank-ai/Real-ESRGAN", "RealESRGAN_x4.pth", local_dir=model_path_resrgan)
     hf_hub_download("sberbank-ai/Real-ESRGAN", "RealESRGAN_x8.pth", local_dir=model_path_resrgan)
+    hf_hub_download("schwgHao/RealESRGAN_x4plus", "RealESRGAN_x4plus.pth", local_dir=model_path_resrgan)
 
     model_resrgan_path  = os.path.join(model_path_resrgan, modelid_resrgan)
     device = torch.device(device_resrgan)
@@ -47,10 +55,10 @@ def image_resrgan(
     model_resrgan.load_weights(model_resrgan_path, download=True)
     image = Image.open(img_resrgan).convert('RGB')
     sr_image = model_resrgan.predict(image)
-    final_image = [] 
+    final_image = []
     savename = name_image()
     if use_gfpgan_resrgan == True :
-        sr_image = image_gfpgan_mini(sr_image)    
+        sr_image = image_gfpgan_mini(sr_image)
     sr_image.save(savename)
     final_image.append(savename)
 
