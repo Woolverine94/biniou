d7e70ffe93e186c950e1a063b468598e0208101d
Woolverine94 <woolverine94@proton.me>
2026-08-18 18:44:14 +0200

    🔥🔧🪶 Bugfix, code enhancement and add support for RestoreFormer in GFPGAN module

--- ressources/gfpgan.py
+++ ressources/gfpgan.py
@@ -21,12 +21,12 @@ model_list_gfpgan = [
 ]
 
 variant_list_gfpgan = [
-    "GFPGANv1.pth",
-    "GFPGANCleanv1-NoCE-C2.pth",    
+#    "GFPGANv1.pth",
+    "GFPGANCleanv1-NoCE-C2.pth",
     "GFPGANv1.2.pth",
     "GFPGANv1.3.pth",
     "GFPGANv1.4.pth",
-#    "RestoreFormer.pth",
+    "RestoreFormer.pth",
 ]
 
 @metrics_decoration
@@ -34,7 +34,12 @@ def image_gfpgan(modelid_gfpgan, variantid_gfpgan, img_gfpgan, progress_gfpgan=g
     print(">>>[GFPGAN 🔎]: starting module")
     path_gfpgan = os.path.join(model_path_gfpgan, variantid_gfpgan)
     device = torch.device(device_gfpgan)
-    
+
+    if variantid_gfpgan == "RestoreFormer.pth":
+        arch_gfpgan = "RestoreFormer"
+    else:
+        arch_gfpgan = "clean"
+
     if os.path.exists(path_gfpgan) == False :
         snapshot_path_gfpgan = snapshot_download(
             repo_id=modelid_gfpgan, 
@@ -43,25 +48,25 @@ def image_gfpgan(modelid_gfpgan, variantid_gfpgan, img_gfpgan, progress_gfpgan=g
             resume_download=True,
 #            local_files_only=True if offline_test() else None
             )
-        path_gfpgan = os.path.join(snapshot_path_gfpgan, variantid_gfpgan)    
-    
+        path_gfpgan = os.path.join(snapshot_path_gfpgan, variantid_gfpgan)
+
     model_gfpgan = GFPGANer(
-        model_path=path_gfpgan, 
+        model_path=path_gfpgan,
         upscale=1, 
-        arch='clean', 
-        channel_multiplier=2
+        arch=arch_gfpgan,
+        channel_multiplier=2,
     )
-    
+
     image_inter_gfpgan = np.array(Image.open(img_gfpgan).convert('RGB'))
     image_input_gfpgan = cv2.cvtColor(image_inter_gfpgan, cv2.COLOR_RGB2BGR)
     
     _, _, image_gfpgan = model_gfpgan.enhance(
-        image_input_gfpgan, 
-        has_aligned=False, 
-        only_center_face=False, 
-        paste_back=True
+        image_input_gfpgan,
+        has_aligned=False,
+        only_center_face=False,
+        paste_back=True,
     )
-    
+
     final_image = []
     savename = name_image()
     image_gfpgan = cv2.cvtColor(image_gfpgan, cv2.COLOR_BGR2RGB)
@@ -85,32 +90,32 @@ def image_gfpgan(modelid_gfpgan, variantid_gfpgan, img_gfpgan, progress_gfpgan=g
 
 def image_gfpgan_mini(img_gfpgan):
     modelid_gfpgan = model_list_gfpgan[0]
-    variantid_gfpgan = variant_list_gfpgan[4]
+    variantid_gfpgan = variant_list_gfpgan[3]
     path_gfpgan = os.path.join(model_path_gfpgan, variantid_gfpgan)
     device = torch.device(device_gfpgan)
-    
+
     if os.path.exists(path_gfpgan) == False :
         snapshot_path_gfpgan = snapshot_download(
             repo_id=modelid_gfpgan, 
             local_dir=model_path_gfpgan, 
             local_dir_use_symlinks=False,
             resume_download=True,
-#            local_files_only=True if offline_test() else None            
+#            local_files_only=True if offline_test() else None
         )
         path_gfpgan = os.path.join(snapshot_path_gfpgan, variantid_gfpgan)
-        
+
     model_gfpgan = GFPGANer(
         model_path=path_gfpgan, 
         upscale=1, 
         arch='clean', 
         channel_multiplier=2
     )
-    
+
     if isinstance(img_gfpgan, np.ndarray):
         image_inter_gfpgan = img_gfpgan
-    else :        
+    else :
         image_inter_gfpgan = np.array(img_gfpgan.convert('RGB'))
-    
+
     image_input_gfpgan = cv2.cvtColor(image_inter_gfpgan, cv2.COLOR_RGB2BGR)
     _, _, image_gfpgan = model_gfpgan.enhance(
         image_input_gfpgan, 
@@ -118,8 +123,8 @@ def image_gfpgan_mini(img_gfpgan):
         only_center_face=False, 
         paste_back=True
     )
-    
-    image_gfpgan = cv2.cvtColor(image_gfpgan, cv2.COLOR_BGR2RGB)    
+
+    image_gfpgan = cv2.cvtColor(image_gfpgan, cv2.COLOR_BGR2RGB)
     image_gfpgan_output = Image.fromarray(image_gfpgan)
 
     del model_gfpgan, image_inter_gfpgan, image_input_gfpgan, image_gfpgan
