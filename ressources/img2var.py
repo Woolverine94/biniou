82a7ea8f546ef8d0749fb4ae653125ede63d3565
Woolverine94 <woolverine94@proton.me>
2026-08-25 18:17:11 +0200

    🪶🔥 Code enhancement and replacement of default model for Image Variation module

--- ressources/img2var.py
+++ ressources/img2var.py
@@ -18,7 +18,8 @@ model_path_img2var = "./models/Stable_Diffusion/"
 os.makedirs(model_path_img2var, exist_ok=True)
 
 model_list_img2var = [
-    "lambdalabs/sd-image-variations-diffusers",
+#    "lambdalabs/sd-image-variations-diffusers",
+    "lambda/sd-image-variations-diffusers",
 ]
 
 # Bouton Cancel
@@ -43,19 +44,19 @@ def check_img2var(step, timestep, latents) :
 
 @metrics_decoration
 def image_img2var(
-    modelid_img2var, 
-    sampler_img2var, 
-    img_img2var, 
-    num_images_per_prompt_img2var, 
-    num_prompt_img2var, 
-    guidance_scale_img2var, 
-    num_inference_step_img2var, 
-    height_img2var, 
-    width_img2var, 
-    seed_img2var, 
-    use_gfpgan_img2var, 
-    nsfw_filter, 
-    tkme_img2var,    
+    modelid_img2var,
+    sampler_img2var,
+    img_img2var,
+    num_images_per_prompt_img2var,
+    num_prompt_img2var,
+    guidance_scale_img2var,
+    num_inference_step_img2var,
+    height_img2var,
+    width_img2var,
+    seed_img2var,
+    use_gfpgan_img2var,
+    nsfw_filter,
+    tkme_img2var,
     progress_img2var=gr.Progress(track_tqdm=True)
     ):
 
@@ -65,22 +66,23 @@ def image_img2var(
 
     pipe_img2var = StableDiffusionImageVariationPipeline.from_pretrained(
         modelid_img2var,
-        revision="v2.0", 
-        cache_dir=model_path_img2var, 
+#        revision="v2.0",
+        revision="main",
+        cache_dir=model_path_img2var,
         torch_dtype=model_arch,
         safety_checker=nsfw_filter_final,
         resume_download=True,
-        local_files_only=True if offline_test() else None                
-        )
+        local_files_only=True if offline_test() else None
+    )
 
     pipe_img2var = schedulerer(pipe_img2var, sampler_img2var)
-    pipe_img2var.enable_attention_slicing("max")  
+    pipe_img2var.enable_attention_slicing("max")
     tomesd.apply_patch(pipe_img2var, ratio=tkme_img2var)
-    if device_label_img2var == "cuda" :
+    if device_label_img2var == "cuda":
         pipe_img2var.enable_sequential_cpu_offload()
-    else : 
+    else :
         pipe_img2var = pipe_img2var.to(device_img2var)
-    
+
     if seed_img2var == 0:
         random_seed = random.randrange(0, 10000000000, 1)
         final_seed = random_seed
@@ -94,21 +96,21 @@ def image_img2var(
     image_input = PIL.Image.open(img_img2var)
     image_input = image_input.convert("RGB")
     image_input = image_input.resize((dim_size[0], dim_size[1]))
-    
+
     final_image = []
-    final_seed = []    
+    final_seed = []
     for i in range (num_prompt_img2var):
-        image = pipe_img2var(        
+        image = pipe_img2var(
             image=image_input,
             num_images_per_prompt=num_images_per_prompt_img2var,
             guidance_scale=guidance_scale_img2var,
             num_inference_steps=num_inference_step_img2var,
             width=dim_size[0],
-            height=dim_size[1],             
-            generator = generator[i], 
-            callback=check_img2var, 
-#            callback_on_step_end=check_img2var, 
-#            callback_on_step_end_tensor_inputs=['latents'], 
+            height=dim_size[1],
+            generator = generator[i],
+            callback=check_img2var,
+#            callback_on_step_end=check_img2var,
+#            callback_on_step_end_tensor_inputs=['latents'],
         ).images
 
         for j in range(len(image)):
@@ -119,7 +121,7 @@ def image_img2var(
             image[j].save(savename)
             final_image.append(savename)
             final_seed.append(seed_id)
-            
+
     print(f">>>[Image variation 🖼️ ]: generated {num_prompt_img2var} batch(es) of {num_images_per_prompt_img2var}")
     reporting_img2var = f">>>[Image variation 🖼️ ]: "+\
         f"Settings : Model={modelid_img2var} | "+\
@@ -131,12 +133,12 @@ def image_img2var(
         f"Token merging={tkme_img2var} | "+\
         f"nsfw_filter={bool(int(nsfw_filter))} | "+\
         f"Seed List="+ ', '.join([f"{final_seed[m]}" for m in range(len(final_seed))])
-    print(reporting_img2var)             
+    print(reporting_img2var)
 
     exif_writer_png(reporting_img2var, final_image)
 
     del nsfw_filter_final, feat_ex, pipe_img2var, generator, image_input, image
     clean_ram()
 
-    print(f">>>[Image variation 🖼️ ]: leaving module")  
-    return final_image, final_image 
+    print(f">>>[Image variation 🖼️ ]: leaving module")
+    return final_image, final_image
