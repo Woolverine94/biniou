de57f35f9850e39fbcdfccf6a61269c1cf03aa88
Woolverine94 <woolverine94@proton.me>
2026-09-15 18:13:01 +0200

    🪶 Code enhancement for MagicMix module

--- ressources/magicmix.py
+++ ressources/magicmix.py
@@ -15,12 +15,12 @@ device_magicmix = torch.device(device_label_magicmix)
 # Gestion des modèles
 model_path_magicmix = "./models/Stable_Diffusion/"
 os.makedirs(model_path_magicmix, exist_ok=True)
-model_list_magicmix = []
+model_list_magicmix_local = []
 
 for filename in os.listdir(model_path_magicmix):
     f = os.path.join(model_path_magicmix, filename)
     if os.path.isfile(f) and (filename.endswith('.ckpt') or filename.endswith('.safetensors')):
-        model_list_magicmix.append(f)
+        model_list_magicmix_local.append(f)
 
 model_list_magicmix_builtin = [
     "SG161222/Realistic_Vision_V3.0_VAE",
@@ -29,8 +29,10 @@ model_list_magicmix_builtin = [
     "nitrosocke/Ghibli-Diffusion", 
 ]
 
-for k in range(len(model_list_magicmix_builtin)):
-    model_list_magicmix.append(model_list_magicmix_builtin[k])
+model_list_magicmix = model_list_magicmix_builtin
+
+for k in range(len(model_list_magicmix_local)):
+    model_list_magicmix.append(model_list_magicmix_local[k])
 
 # Bouton Cancel
 stop_magicmix = False
