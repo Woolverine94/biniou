39ca544562322a8b8d3f308beef961bb4a86ad72
Woolverine94 <woolverine94@proton.me>
2026-09-21 18:38:20 +0200

    🔥 Add support for Chatbot high-end model sdkyuan/qwen3.8-27B-qat-q2_0-gguf

--- ressources/llamacpp.py
+++ ressources/llamacpp.py
@@ -230,6 +230,7 @@ model_list_llamacpp_builtin = {
     "bartowski/OpenBuddy_OpenBuddy-R1-0528-Distill-Qwen3-32B-Preview0-QAT-GGUF":("OpenBuddy_OpenBuddy-R1-0528-Distill-Qwen3-32B-Preview0-QAT-Q5_K_M.gguf", "<|role|>system<|says|>{system_prompt}<|end|>\n<|role|>user<|says|>{prompt}<|end|>\n<|role|>assistant<|says|>", std_system_prompt),
     "OpenBuddy/SimpleChat-72B-V3-QAT-GGUF":("ggml-model-Q2_K.gguf", prompt_template_list_llamacpp["Qwen 3"][0], std_system_prompt),
     "DavidAU/Qwen3.8-27B-TURBO-Fable-Cold-Fusion-735-882-Heretic-Uncensored-NEO-CODER-MAX-MTP-GGUF":("Qwen3.8-27B-TurboFCFusion-735-882-Here-Uncen-NEO-CODER-MAX-Q5_K_M.gguf", prompt_template_list_llamacpp["Qwen 3.8"][0], std_system_prompt),
+    "sdkyuan/qwen3.8-27B-qat-q2_0-gguf":("qwen38-27b-qat-q2_0.gguf", prompt_template_list_llamacpp["Qwen 3.8"][0], std_system_prompt),
     "bartowski/Qwen3.8-27B-GGUF":("Qwen3.8-27B-Q5_K_M.gguf", prompt_template_list_llamacpp["Qwen 3.8"][0], std_system_prompt),
     "bartowski/thomsonreuters_Thomson-1.0-Small-GGUF":("thomsonreuters_Thomson-1.0-Small-Q5_K_M.gguf", prompt_template_list_llamacpp["Qwen 3.5"][0], std_system_prompt),
     "bartowski/Qwen_Qwen3.6-35B-A3B-GGUF":("Qwen_Qwen3.6-35B-A3B-Q5_K_M.gguf", prompt_template_list_llamacpp["Qwen 3.5"][0], std_system_prompt),
